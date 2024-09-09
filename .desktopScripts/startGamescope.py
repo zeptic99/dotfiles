afbee9e80c913bb082a0a10475259ab048ee8e93
zeptic99 <taseenrashed@gmail.com>
2024-09-10 05:11:10 +0600

    Added screen res functions

--- .desktopScripts/startGamescope.py
+++ .desktopScripts/startGamescope.py
@@ -5,42 +5,53 @@ command = "MANGOHUD_CONFIGFILE=$HOME/.config/MangoHud/MangoHud.conf vblank_mode=
 
 root = tk.Tk()
 root.geometry("400x400")
+
 # Define the button functions
-def button1_click():
-    # Disable the other buttons
-    button2.config(state="disabled")
-    button3.config(state="disabled")
-    button4.config(state="disabled")
 
-def button2_click():
-    # Disable the other buttons
-    button1.config(state="disabled")
-    button3.config(state="disabled")
-    button4.config(state="disabled")
+##############
+# Screen res #
+##############
 
-def button3_click():
-    # Disable the other buttons
-    button1.config(state="disabled")
-    button2.config(state="disabled")
-    button4.config(state="disabled")
 
-def button4_click():
-    # Disable the other buttons
-    button1.config(state="disabled")
-    button2.config(state="disabled")
-    button3.config(state="disabled")
+def sc_1920():
+    global screenRes
+    screenRes = " -w 1920 -h 1080"
+
+
+def sc_1440():
+    global screenRes
+    screenRes = " -w 1920 -h 1080"
+
+
+def sc_1080():
+    global screenRes
+    screenRes = " -w 1920 -h 1080"
+
+
+def sc_4k():
+    global screenRes
+    screenRes = " -w 1920 -h 1080"
 
 # Create the buttons
-button1 = tk.Button(root, text="Button 1", command=button1_click)
-button2 = tk.Button(root, text="Button 2", command=button2_click)
-button3 = tk.Button(root, text="Button 3", command=button3_click)
-button4 = tk.Button(root, text="Button 4", command=button4_click)
+
+
+button1 = tk.Button(root, text="1920", command=sc_1920)
+button2 = tk.Button(root, text="1440", command=sc_1440)
+button3 = tk.Button(root, text="1080", command=sc_1080)
+button4 = tk.Button(root, text="4k", command=sc_4k)
 
 # Pack the buttons
 button1.pack()
 button2.pack()
 button3.pack()
 button4.pack()
+
+command += screenRes
+command += gameRes
+command += scaling
+command += monitor
+
+
 root.mainloop()
 
 os.system(command)
